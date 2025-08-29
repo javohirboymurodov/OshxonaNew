568c781610c75005b1d1cde72c0c4dcb73b2f872
Javohir <javohirboy0304@gmail.com>
2025-08-29 10:08:44 +0500

    user frontendni nastroykasi to'g'irlandi

--- userfront/vite.config.ts
+++ userfront/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3003
+  },
+  // Vercel uchun qo'shimcha sozlamalar
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  preview: {
+    port: 3003
   }
 })
-
-
